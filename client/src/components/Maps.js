b5e32b7bb2ea58069e0d68e43176515493079047
Suyog <76236385+Suyog-Raut@users.noreply.github.com>
2022-08-02 23:08:34 +0530

    Add files via upload

--- client/src/components/Maps.js
+++ client/src/components/Maps.js
@@ -1,41 +1,45 @@
-import {useState,useEffect} from 'react';
-import ElementCard from './ElementCard';
-
+import { useState, useEffect } from "react";
+import ElementCard from "./ElementCard";
 
 function Maps() {
+  const [Data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  const [Data,setData] = useState([]);
- const [loading,setLoading] = useState(true);
+  useEffect(() => {
+    const apiUrl = "/api/maps";
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data.data);
+        console.log(data.data);
+      })
+      .catch((error) => console.log(error))
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [setData, setLoading]);
 
+  if (loading) return <p>Loading...</p>;
 
-  useEffect(()=> {
-    const apiUrl = "/maps";
-       fetch(apiUrl)
-         .then((res) => res.json())
-         .then((data) => {
-           setData(data.data);
-           console.log(data.data);
-         }).catch((error) => console.log(error)).finally(()=>{
-           setLoading(false);
-         });
-  },[setData,setLoading])
+  if (!Data) return <p>Server down :(</p>;
 
-if(loading) return <p>Loading...</p>
+  return (
+    <div className="center">
+      <div className="">
+        <h2 className="outline-text">Maps</h2>
 
-if(!Data) return <p>Server down :(</p>
-
- return (  <div className="center">
-<div className="">
-<h2 className="outline-text">Maps</h2>
-
-{ Data.map(s =>
-
-  <ElementCard name={s.displayName} img={s.splash} coord={s.coordinates} icon={s.displayIcon}/>
-  ) }
-  <hr/>
-   </div>
-</div>
- );
+        {Data.map((s) => (
+          <ElementCard
+            name={s.displayName}
+            img={s.splash}
+            coord={s.coordinates}
+            icon={s.displayIcon}
+          />
+        ))}
+        <hr />
+      </div>
+    </div>
+  );
 }
 
-export default Maps ;
+export default Maps;
